81ea9df0b371e9d2fa96ff28b306f0a030b2ed41
antonklyuchnikov1990 <antonklyuchnikov1990@gmail.com>
2022-10-17 12:56:30 +0200

    Parsing HomeWork

--- HM_12.10.2022_StreamAPI/src/Main.java
+++ HM_12.10.2022_StreamAPI/src/Main.java
@@ -27,13 +27,44 @@ public class Main {
 //Output: -1
 //Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
-        String needle = "ght";
-        String haystack = "klldsjfksdjfisdfj";
+        String needle = "sad";
+        String haystack = "sabutsad";
         System.out.println(findIndex(needle, haystack));
+        System.out.println(kmp(needle, haystack));
 
+    }
+
+    private static int kmp(String needle, String haystack) {
+        int len = needle.length();
+        int[] pi = prefix(needle + "#" + haystack);
+        for (int i = 0; i < haystack.length(); i++) {
+            if (pi[i + 1 + len] == len) {
+                return i - len + 1;
+            }
+        }
+        return -1;
     }
 
     private static int findIndex(String s, String str) {
         return str.indexOf(s);
     }
+
+    private static int[] prefix(String str) {
+        int len = str.length();
+        int[] pi = new int[len];
+        pi[0] = 0;
+        for (int i = 1; i < len; i++) {
+            int j = pi[i - 1];
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (str.charAt(i) == str.charAt(j)) {
+                pi[i] = j + 1;
+            } else {
+                pi[i] = j;
+            }
+        }
+        return pi;
+    }
+
 }
