1a3df901da96422523339a9f56ac7b0b21d1d169
antonklyuchnikov1990 <antonklyuchnikov1990@gmail.com>
2022-11-07 12:47:37 +0100

    Maven

---
+++ junit/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("12344");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1,2));
+    }
+}
