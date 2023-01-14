a2a1f544047d31830cf52ef68fdadf46db0fbd4f
antonklyuchnikov1990 <antonklyuchnikov1990@gmail.com>
2023-01-14 23:35:27 +0100

    Download debts

---
+++ hellloSpringBoot/src/main/java/de/telran/hellloSpringBoot/HellloSpringBootApplication.java
@@ -0,0 +1,13 @@
+package de.telran.hellloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellloSpringBootApplication.class, args);
+	}
+
+}
